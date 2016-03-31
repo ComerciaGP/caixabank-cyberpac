386a4bd1672dfa552a976d066a77fbfc3218f740
José Conti <j.conti@joseconti.com>
2016-04-01 00:50:00 +0200

    Updating tgm loader

--- includes/tgm/tgm-loader.php
+++ includes/tgm/tgm-loader.php
@@ -45,55 +45,27 @@ function caixabank__register_required_plugins() {
 	$plugins = array(
 
 		// This is an example of how to include a plugin bundled with a theme.
-		array(
-			'name'               => 'TGM Example Plugin', // The plugin name.
-			'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-			'source'             => dirname( __FILE__ ) . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
-
-		// This is an example of how to include a plugin from an arbitrary external source in your theme.
-		array(
-			'name'         => 'TGM New Media Plugin', // The plugin name.
-			'slug'         => 'tgm-new-media-plugin', // The plugin slug (typically the folder name).
-			'source'       => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', // The plugin source.
-			'required'     => true, // If false, the plugin is only 'recommended' instead of required.
-			'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
-		),
-
-		// This is an example of how to include a plugin from a GitHub repository in your theme.
-		// This presumes that the plugin code is based in the root of the GitHub repository
-		// and not in a subdirectory ('/src') of the repository.
-		array(
-			'name'      => 'Adminbar Link Comments to Pending',
-			'slug'      => 'adminbar-link-comments-to-pending',
-			'source'    => 'https://github.com/jrfnl/WP-adminbar-comments-to-pending/archive/master.zip',
-		),
-
 		// This is an example of how to include a plugin from the WordPress Plugin Repository.
 		array(
-			'name'      => 'BuddyPress',
-			'slug'      => 'buddypress',
+			'name'      => 'WooCommerce',
+			'slug'      => 'woocommerce',
 			'required'  => false,
 		),
-
-		// This is an example of the use of 'is_callable' functionality. A user could - for instance -
-		// have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.
-		// 'wordpress-seo-premium'.
-		// By setting 'is_callable' to either a function from that plugin or a class method
-		// `array( 'class', 'method' )` similar to how you hook in to actions and filters, TGMPA can still
-		// recognize the plugin as being installed.
 		array(
-			'name'        => 'WordPress SEO by Yoast',
-			'slug'        => 'wordpress-seo',
-			'is_callable' => 'wpseo_init',
+			'name'      => 'WangGuard',
+			'slug'      => 'wangguard',
+			'required'  => false,
 		),
-
+		array(
+			'name'      => 'Yoast SEO',
+			'slug'      => 'wordpress-seo',
+			'required'  => false,
+		),
+		array(
+			'name'      => 'Shortcake (Shortcode UI)',
+			'slug'      => 'shortcode-ui',
+			'required'  => false,
+		),
 	);
 
 	/*
@@ -109,7 +81,7 @@ function caixabank__register_required_plugins() {
 		'id'           => 'caixabank-tools-official',                 // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
 		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'plugins.php',            // Parent menu slug.
+		'parent_slug'  => 'caixabank',            // Parent menu slug.
 		'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
 		'has_notices'  => true,                    // Show admin notices or not.
 		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
@@ -117,10 +89,9 @@ function caixabank__register_required_plugins() {
 		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
 		'message'      => '',                      // Message to output right before the plugins table.
 
-		/*
 		'strings'      => array(
-			'page_title'                      => __( 'Install Required Plugins', 'caixabank-tools-official' ),
-			'menu_title'                      => __( 'Install Plugins', 'caixabank-tools-official' ),
+			'page_title'                      => __( 'Install Recommended Plugins', 'caixabank-tools-official' ),
+			'menu_title'                      => __( 'Recommended Plugins', 'caixabank-tools-official' ),
 			'installing'                      => __( 'Installing Plugin: %s', 'caixabank-tools-official' ), // %s = plugin name.
 			'oops'                            => __( 'Something went wrong with the plugin API.', 'caixabank-tools-official' ),
 			'notice_can_install_required'     => _n_noop(
@@ -129,8 +100,8 @@ function caixabank__register_required_plugins() {
 				'caixabank-tools-official'
 			), // %1$s = plugin name(s).
 			'notice_can_install_recommended'  => _n_noop(
-				'This theme recommends the following plugin: %1$s.',
-				'This theme recommends the following plugins: %1$s.',
+				'CaixaBank recommends the following plugin: %1$s.',
+				'CaixaBank recommends the following plugins: %1$s.',
 				'caixabank-tools-official'
 			), // %1$s = plugin name(s).
 			'notice_cannot_install'           => _n_noop(
@@ -193,7 +164,6 @@ function caixabank__register_required_plugins() {
 
 			'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
 		),
-		*/
 	);
 
 	tgmpa( $plugins, $config );
