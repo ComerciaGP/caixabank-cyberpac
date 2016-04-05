39ac5aae75bc864b3adfc570086628219b7309a9
José Conti <j.conti@joseconti.com>
2016-04-05 22:05:11 +0200

    Page setting

--- core/pages/caixabank-settings.php
+++ core/pages/caixabank-settings.php
@@ -5,11 +5,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 function caixabank_settings(){ ?>
     <div class="wrap">
         <h1><?php echo __( 'CaixaBank Tools Official Settings', 'caixabank-tools-official' ) ?></h1>
+
+        <?php
+            if( isset( $_GET[ 'tab' ] ) ) {
+                $active_tab = $_GET[ 'tab' ];
+            } else {
+	            $active_tab = 'general_options';
+            }
+        ?>
+
+        <h2 class="nav-tab-wrapper">
+			<a href="?page=caixabank&tab=general_options" class="nav-tab <?php echo $active_tab == 'general_options' ? 'nav-tab-active' : ''; ?>"><?php _e( 'General Settings', 'caixabank-tolls-official' ); ?></a>
+            <a href="?page=caixabank&tab=gateway_options" class="nav-tab <?php echo $active_tab == 'gateway_options' ? 'nav-tab-active' : ''; ?>"><?php _e( 'CaixaBank Gateway Settings', 'caixabank-tolls-official' ); ?></a>
+		</h2>
+
         <form method="post" action="options.php">
             <?php
-                settings_fields("caixabank-settings-section");
-                do_settings_sections("caixabank-settings-options");
-                submit_button();
+	            if( $active_tab == 'general_options' ) {
+		            settings_fields( "caixabank-general-section" );
+		            do_settings_sections( "caixabank-general-options" );
+		            }
+				elseif ( $active_tab == 'gateway_options' ) {
+					settings_fields( "caixabank-gateway-section" );
+		            do_settings_sections( "caixabank-gateway-options" );
+		            }
+
+
+
+		            submit_button();
             ?>
         </form>
         </div>
@@ -53,121 +76,25 @@ function caixabank_settings(){ ?>
       if ( dnimandatorydisabled ) {
           var switchery = new Switchery(dnimandatorydisabled, { size: 'small', disabled: true });
       }
-
-
-
-
-
-
-
-
-      var widget = document.querySelector('.js-switch-widget');
-      if ( widget ) {
-          var switchery = new Switchery(widget, { size: 'small' });
+      var irpf = document.querySelector('.js-switch-irpf');
+      if ( irpf ) {
+          var switchery = new Switchery(irpf, { size: 'small' });
       }
-      var disabledOpacity = document.querySelector('.js-switch-disabled-opacity');
-      if ( disabledOpacity ) {
-          var switchery = new Switchery(disabledOpacity, { disabled: true, disabledOpacity: 0.75 });
+	  var secondterminal = document.querySelector('.js-switch-secondterminal');
+      if ( secondterminal ) {
+          var switchery = new Switchery(secondterminal, { size: 'small' });
       }
-      // Colored switches
-      var blue = document.querySelector('.js-switch-blue');
-      if ( blue ) {
-          var switchery = new Switchery(blue, { color: '#7c8bc7', jackColor: '#9decff' });
+     var testmode = document.querySelector('.js-switch-testmode');
+      if ( testmode ) {
+          var switchery = new Switchery(testmode, { size: 'small' });
       }
-      // Switch sizes
-      var small = document.querySelector('.js-switch-small');
-      if ( small ) {
-          var switchery = new Switchery(small, { size: 'small' });
-      }
-      var large = document.querySelector('.js-switch-large');
-      if ( large ) {
-          var switchery = new Switchery(large, { size: 'large' });
-      }
-      // Dynamic enable/disable
-      var dynamicDisable = document.querySelector('.js-dynamic-state');
-      if ( dynamicDisable ) {
-          var dynamicStateCheckbox = new Switchery(dynamicDisable);
-      }
-      var dynamicDisable = document.querySelector('.js-dynamic-disable');
-      if ( dynamicDisable ) {
-        dynamicDisable.addEventListener('click', function() {
-            dynamicStateCheckbox.disable();
-        });
-      }
-      var dynamicEnable = document.querySelector('.js-dynamic-enable');
-      if ( dynamicEnable ) {
-        dynamicEnable.addEventListener('click', function() {
-            dynamicStateCheckbox.enable();
-        });
-      }
-      // Getting checkbox state
-      // On click
-      var clickCheckbox = document.querySelector('.js-check-click')
-        , clickButton = document.querySelector('.js-check-click-button');
-      if (window.addEventListener && clickButton) {
-        clickButton.addEventListener('click', function() {
-          alert(clickCheckbox.checked);
-        });
-      } else if ( ! window.addEventListener && clickButton) {
-        clickButton.attachEvent('onclick', function() {
-          alert(clickCheckbox.checked);
-        });
-      }
-      // On change
-      var changeCheckbox = document.querySelector('.js-check-change')
-        , changeField = document.querySelector('.js-check-change-field');
-      if ( changeCheckbox ) {
-          changeCheckbox.onchange = function() {
-              changeField.innerHTML = changeCheckbox.checked;
-          };
-      }
-      // CSS3 Pie for <IE9
-      if (window.PIE) {
-        var wrapper = document.querySelectorAll('.switchery')
-          , handle = document.querySelectorAll('.switchery > small');
-        if (wrapper.length == handle.length) {
-          for (var i = 0; i < wrapper.length; i++) {
-            PIE.attach(wrapper[i]);
-            PIE.attach(handle[i]);
-          }
-        }
+	 var debug = document.querySelector('.js-switch-debug');
+      if ( debug ) {
+          var switchery = new Switchery(debug, { size: 'small' });
       }
     </script>
 <?php }
-function caixabank_dni_field(){ ?>
-        <input type="checkbox" class="js-switch-dni" data-tool="Add DNI field to profile user" name="caixabank_dni_field" value="1" <?php checked(1, get_option('caixabank_dni_field'), true); ?> />
-<?php }
 
-function caixabank_dni_field_required(){ ?>
-        <input type="checkbox" class="js-switch-dnimandatory" data-tool="Activating this option, the DNI will be required" name="caixabank_dni_field_required" value="1" <?php checked(1, get_option('caixabank_dni_field_required'), true); ?> />
-<?php }
-function caixabank_dni_field_required_deactivated(){ ?>
-        <input type="checkbox" class="js-switch-dnimandatorydisabled" data-tool="Activating this option, the DNI will be required" name="caixabank_dni_field_required" value="1" <?php checked(1, get_option('caixabank_dni_field_required'), true); ?> />
-<?php }
-
-
-
-
-
-function caixabank_display_donation_widget(){ ?>
-        <input type="checkbox" class="js-switch-widget" data-tool="Activate donation Widget" name="caixabank_donation_widget" value="1" <?php checked(1, get_option('caixabank_donation_widget'), true); ?> />
-<?php }
-function display_caixabank_panel_fields(){
-	$caixabank_add_dni = get_option( 'caixabank_dni_field' );
-    add_settings_section("caixabank-settings-section", "All Settings", null, "caixabank-settings-options");
-    add_settings_field('caixabank_dni_field', 'Activate the DNI at Checkout <a href="#" data-tool="When you activate this option, a DNI field will be shown at checkout" class="tooltip animate">?</a>', 'caixabank_dni_field', 'caixabank-settings-options', 'caixabank-settings-section');
-    if( $caixabank_add_dni ) add_settings_field('caixabank_dni_field_required', 'The DNI is required <a href="#" data-tool="Activating this option, the DNI will be required" class="tooltip animate">?</a>', 'caixabank_dni_field_required', 'caixabank-settings-options', 'caixabank-settings-section');
-    if( !$caixabank_add_dni ) add_settings_field('caixabank_dni_field_required_deactivated', 'The DNI is required <a href="#" data-tool="Please, first activate the DNI field. Activating this option, the DNI will be required" class="tooltip animate">?</a>', 'caixabank_dni_field_required_deactivated', 'caixabank-settings-options', 'caixabank-settings-section');
-
-    add_settings_field('caixabank_donation_widget', 'Do you want activate the donation widget?', 'caixabank_display_donation_widget', 'caixabank-settings-options', 'caixabank-settings-section');
-
-
-    register_setting('caixabank-settings-section', 'caixabank_dni_field');
-    register_setting('caixabank-settings-section', 'caixabank_dni_field_required');
-    register_setting('caixabank-settings-section', 'caixabank_dni_field_required_deactivated');
-    register_setting('caixabank-settings-section', 'caixabank_donation_widget');
-}
-add_action('admin_init', 'display_caixabank_panel_fields');
 function caixabank_settings_load_js(){
     wp_enqueue_script(  'caixabank_switchery',  CAIXABANK_DIR_URL . '/assets/js/switchery.min.js',  CAIXABANK_TOOLS_OFFICIAL_VERSION );
     //wp_enqueue_script(    'caixabank_check',      CAIXABANK_DIR_URL . '/assets/js/check.js',      CAIXABANK_TOOLS_OFFICIAL_VERSION );
@@ -184,4 +111,11 @@ function caixabank_settings_load_css($hook){
     }
 }
 add_action( 'admin_enqueue_scripts', 'caixabank_settings_load_css' );
+
+
+
+//Include all options
+
+include_once('setting-options/general-settings.php');
+include_once('setting-options/gateway-settings.php');
 ?>
