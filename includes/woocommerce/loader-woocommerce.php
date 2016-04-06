921bc396996617169d06f3ed15afb86a458e4f6b
José Conti <j.conti@joseconti.com>
2016-04-06 03:01:25 +0200

    Removed Invoices from WooCommerce settings

--- includes/woocommerce/loader-woocommerce.php
+++ includes/woocommerce/loader-woocommerce.php
@@ -2,5 +2,4 @@
 	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	require_once plugin_dir_path( __FILE__ ) . 'includes/caixabank-tools-official-woocommerce.php';
 	require_once plugin_dir_path( __FILE__ ) . 'includes/caixabank-tools-official-iupay.php';
-	require_once plugin_dir_path( __FILE__ ) . 'includes/caixabank-tools-official-sort-invoice.php';
 ?>
